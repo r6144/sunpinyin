38049a229f066d916658a02ff3b617422b90fed3
r6144 <rainy6144@gmail.com>
2011-02-21 17:20:33 +0800

    Added build command lines that sets rpath properly.

---
+++ build.sh
@@ -0,0 +1,1 @@
+scons -j4 --prefix=/home/r6144/apps/sunpinyin-git/ install
---
+++ wrapper/ibus/build-for-ibus.sh
@@ -0,0 +1,1 @@
+PKG_CONFIG_PATH=/home/r6144/apps/sunpinyin-git/lib/pkgconfig/ scons -j4 --prefix=/home/r6144/apps/sunpinyin-git/ --rpath=/home/r6144/apps/sunpinyin-git/lib/ install
